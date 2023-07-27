79af1969a03245501694cb315d3d8dc135c813c9
Muhammad Shoaib <muhammad.shoaib@crowdbotics.com>
2023-07-27 23:53:52 +0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

---
+++ screens/Untitled1/index.js
@@ -0,0 +1,27 @@
+import { profileconnect_get_api_v1_profile_list } from "../../store/profileconnect/profileconnect_response_get_Getprofiles.slice.js";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import React from "react";
+import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
+
+const Untitled1 = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(profileconnect_get_api_v1_profile_list());
+  }, []);
+  return <SafeAreaView style={styles.safeArea}>
+        <ScrollView contentContainerStyle={{
+      backgroundColor: '#f0f0f1',
+      padding: 10,
+      position: 'relative',
+      flex: 1
+    }}></ScrollView>
+    </SafeAreaView>;
+};
+
+const styles = StyleSheet.create({
+  safeArea: {
+    height: '100%'
+  }
+});
+export default Untitled1;
