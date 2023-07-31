663714f3ded8dccae39ae42d604853c4fd459758
shahraiz-cb <shahraiz.ali@crowdbotics.com>
2023-07-31 18:26:07 +0200

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled2/index.js
+++ screens/Untitled2/index.js
@@ -1,8 +1,13 @@
+import { profileconnect_post_api_v1_profile_create } from "../../store/profileconnect/profileconnect_response_post_CreateProfiles.slice.js";
+import { Pressable } from "react-native";
 import { useSelector, useDispatch } from "react-redux";
 import React, { useState } from "react";
 import { StyleSheet, ScrollView, SafeAreaView, TextInput, Button, View, Text } from "react-native";
 
 const Untitled2 = () => {
+  const {
+    entities: Name
+  } = useSelector(state => state.Name);
   const {
     entities: Profileconnect_response_post_CreateProfiles
   } = useSelector(state => state.Profileconnect_response_post_CreateProfiles);
@@ -13,6 +18,13 @@ const Untitled2 = () => {
   const handleSubmit = () => {
     // Handle form submission logic here
     dispatch(Profileconnect_response_post_CreateProfiles({
+      name,
+      description
+    }));
+  };
+
+  const onSubmit = () => {
+    dispatch(profileconnect_post_api_v1_profile_create({
       name,
       description
     }));
@@ -30,6 +42,11 @@ const Untitled2 = () => {
           <TextInput style={styles.input} placeholder="Enter a description" value={description} onChangeText={setDescription} data={[1, 2, 3]} />
         </View>
         <Button title="Submit" onPress={handleSubmit} />
+        <Pressable onPress={onSubmit}>
+          <View style={styles.KMzsglfk}>
+            <Text style={styles.MkUDRBOZ}>Lorem ipsum…</Text>
+          </View>
+        </Pressable>
       </ScrollView>
     </SafeAreaView>;
 };
@@ -61,6 +78,24 @@ const styles = StyleSheet.create({
     borderColor: "gray",
     borderWidth: 1,
     paddingHorizontal: 10
+  },
+  KMzsglfk: {
+    height: 60,
+    width: 140,
+    backgroundColor: "#E4E4E4",
+    borderRadius: 0,
+    color: "#777777"
+  },
+  MkUDRBOZ: {
+    width: 100,
+    height: 24,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0,
+    textAlign: "center",
+    position: "absolute",
+    left: 20,
+    top: 18
   }
 });
 export default Untitled2;
