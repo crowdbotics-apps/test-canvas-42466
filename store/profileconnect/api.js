0a12ffa22252be2d6fdbc1755458daefccc951e1
Crowdbotics <engineering@crowdbotics.com>
2023-07-31 15:26:26 +0000

    Update store.

--- store/profileconnect/api.js
+++ store/profileconnect/api.js
@@ -1,16 +1,15 @@
-import axios from "axios";
+import axios from "axios"
 const profileconnect = axios.create({
   baseURL: "https://test-canvas-42466.botics.co",
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json"
-  }
-});
-
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
 function profileconnect_get_api_v1_profile_list(payload) {
-  return profileconnect.get(`/api/v1/profile/`);
+  return profileconnect.get(`/api/v1/profile/`)
 }
-
+function profileconnect_post_api_v1_profile_create(payload) {
+  return profileconnect.post(`/api/v1/profile/`, payload.data)
+}
 export const apiService = {
-  profileconnect_get_api_v1_profile_list
-};
+  profileconnect_get_api_v1_profile_list,
+  profileconnect_post_api_v1_profile_create
+}
