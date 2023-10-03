846d108686304adc5fbb40105fb7ba72dda19f23
Muhammad Shoaib <muhammad.shoaib@crowdbotics.com>
2023-10-03 23:26:21 +0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- store/profileconnect/api.js
+++ store/profileconnect/api.js
@@ -1,15 +1,21 @@
-import axios from "axios"
+import axios from "axios";
 const profileconnect = axios.create({
   baseURL: "https://test-canvas-42466.botics.co",
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
-})
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json"
+  }
+});
+
 function profileconnect_get_api_v1_profile_list(payload) {
-  return profileconnect.get(`/api/v1/profile/`)
+  return profileconnect.get(`/api/v1/profile/`);
 }
+
 function profileconnect_post_api_v1_profile_create(payload) {
-  return profileconnect.post(`/api/v1/profile/`, payload)
+  return profileconnect.post(`/api/v1/profile/`, payload);
 }
+
 export const apiService = {
   profileconnect_get_api_v1_profile_list,
   profileconnect_post_api_v1_profile_create
-}
+};
