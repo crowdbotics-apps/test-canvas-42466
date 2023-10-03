6ee6a7a0804aee4d404295c961504c72eba768da
Muhammad Shoaib <muhammad.shoaib@crowdbotics.com>
2023-10-03 17:23:23 +0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- store/testcanvasAPI/api.js
+++ store/testcanvasAPI/api.js
@@ -1,70 +1,96 @@
-import axios from "axios"
+import axios from "axios";
 const testcanvasAPI = axios.create({
   baseURL: "https://test-canvas-42466.botics.co",
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
-})
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json"
+  }
+});
+
 function api_docs_schema_retrieve(payload) {
   return testcanvasAPI.get(`/api-docs/schema/`, {
-    params: { lang: payload.lang }
-  })
+    params: {
+      lang: payload.lang
+    }
+  });
 }
+
 function api_v1_login_create(payload) {
-  return testcanvasAPI.post(`/api/v1/login/`, payload)
+  return testcanvasAPI.post(`/api/v1/login/`, payload);
 }
+
 function api_v1_profile_list(payload) {
-  return testcanvasAPI.get(`/api/v1/profile/`)
+  return testcanvasAPI.get(`/api/v1/profile/`);
 }
+
 function api_v1_profile_create(payload) {
-  return testcanvasAPI.post(`/api/v1/profile/`, payload)
+  return testcanvasAPI.post(`/api/v1/profile/`, payload);
 }
+
 function api_v1_profile_retrieve(payload) {
-  return testcanvasAPI.get(`/api/v1/profile/${payload.id}/`)
+  return testcanvasAPI.get(`/api/v1/profile/${payload.id}/`);
 }
+
 function api_v1_profile_update(payload) {
-  return testcanvasAPI.put(`/api/v1/profile/${payload.id}/`, payload)
+  return testcanvasAPI.put(`/api/v1/profile/${payload.id}/`, payload);
 }
+
 function api_v1_profile_partial_update(payload) {
-  return testcanvasAPI.patch(`/api/v1/profile/${payload.id}/`, payload)
+  return testcanvasAPI.patch(`/api/v1/profile/${payload.id}/`, payload);
 }
+
 function api_v1_profile_destroy(payload) {
-  return testcanvasAPI.delete(`/api/v1/profile/${payload.id}/`)
+  return testcanvasAPI.delete(`/api/v1/profile/${payload.id}/`);
 }
+
 function api_v1_signup_create(payload) {
-  return testcanvasAPI.post(`/api/v1/signup/`, payload)
+  return testcanvasAPI.post(`/api/v1/signup/`, payload);
 }
+
 function rest_auth_login_create(payload) {
-  return testcanvasAPI.post(`/rest-auth/login/`, payload)
+  return testcanvasAPI.post(`/rest-auth/login/`, payload);
 }
+
 function rest_auth_logout_retrieve(payload) {
-  return testcanvasAPI.get(`/rest-auth/logout/`)
+  return testcanvasAPI.get(`/rest-auth/logout/`);
 }
+
 function rest_auth_logout_create(payload) {
-  return testcanvasAPI.post(`/rest-auth/logout/`)
+  return testcanvasAPI.post(`/rest-auth/logout/`);
 }
+
 function rest_auth_password_change_create(payload) {
-  return testcanvasAPI.post(`/rest-auth/password/change/`, payload)
+  return testcanvasAPI.post(`/rest-auth/password/change/`, payload);
 }
+
 function rest_auth_password_reset_create(payload) {
-  return testcanvasAPI.post(`/rest-auth/password/reset/`, payload)
+  return testcanvasAPI.post(`/rest-auth/password/reset/`, payload);
 }
+
 function rest_auth_password_reset_confirm_create(payload) {
-  return testcanvasAPI.post(`/rest-auth/password/reset/confirm/`, payload)
+  return testcanvasAPI.post(`/rest-auth/password/reset/confirm/`, payload);
 }
+
 function rest_auth_registration_create(payload) {
-  return testcanvasAPI.post(`/rest-auth/registration/`, payload)
+  return testcanvasAPI.post(`/rest-auth/registration/`, payload);
 }
+
 function rest_auth_registration_verify_email_create(payload) {
-  return testcanvasAPI.post(`/rest-auth/registration/verify-email/`, payload)
+  return testcanvasAPI.post(`/rest-auth/registration/verify-email/`, payload);
 }
+
 function rest_auth_user_retrieve(payload) {
-  return testcanvasAPI.get(`/rest-auth/user/`)
+  return testcanvasAPI.get(`/rest-auth/user/`);
 }
+
 function rest_auth_user_update(payload) {
-  return testcanvasAPI.put(`/rest-auth/user/`, payload)
+  return testcanvasAPI.put(`/rest-auth/user/`, payload);
 }
+
 function rest_auth_user_partial_update(payload) {
-  return testcanvasAPI.patch(`/rest-auth/user/`, payload)
+  return testcanvasAPI.patch(`/rest-auth/user/`, payload);
 }
+
 export const apiService = {
   api_docs_schema_retrieve,
   api_v1_login_create,
@@ -86,4 +112,4 @@ export const apiService = {
   rest_auth_user_retrieve,
   rest_auth_user_update,
   rest_auth_user_partial_update
-}
+};
