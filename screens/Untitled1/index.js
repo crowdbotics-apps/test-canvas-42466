487a7e87240f9fadd2ff1f7ee6b38c7ee431587c
Muhammad Shoaib <muhammad.shoaib@crowdbotics.com>
2023-10-05 17:35:20 +0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled1/index.js
+++ screens/Untitled1/index.js
@@ -1,9 +1,17 @@
+import { profileconnect_post_api_v1_profile_create } from "../../store/profileconnect/profileconnect_response_post_CreateProfiles.slice.js";
+import { Pressable } from "react-native";
 import React, { useEffect } from "react";
 import { StyleSheet, ScrollView, SafeAreaView, FlatList, Text, View } from "react-native";
 import { useDispatch, useSelector } from "react-redux";
 import { profileconnect_get_api_v1_profile_list } from "../../store/profileconnect/profileconnect_response_get_Getprofiles.slice.js";
 
 const Untitled1 = () => {
+  const {
+    entities: b
+  } = useSelector(state => state.b);
+  const {
+    entities: a
+  } = useSelector(state => state.a);
   const dispatch = useDispatch();
   const {
     entities
@@ -19,6 +27,13 @@ const Untitled1 = () => {
       <Text style={styles.description}>{item.description}</Text>
     </View>;
 
+  const onSubmit = () => {
+    dispatch(profileconnect_post_api_v1_profile_create({
+      a,
+      b
+    }));
+  };
+
   return <SafeAreaView style={styles.safeArea}>
       <ScrollView contentContainerStyle={{
       backgroundColor: "#f0f0f1",
@@ -27,7 +42,7 @@ const Untitled1 = () => {
       flex: 1
     }}>
         <FlatList data={entities} renderItem={renderItem} keyExtractor={item => item.id.toString()} />
-      </ScrollView>
+      <Pressable onPress={onSubmit}><View style={styles.HqRmAHZM}></View></Pressable></ScrollView>
     </SafeAreaView>;
 };
 
@@ -48,6 +63,13 @@ const styles = StyleSheet.create({
   description: {
     fontSize: 14,
     marginTop: 5
+  },
+  HqRmAHZM: {
+    height: 60,
+    width: 140,
+    backgroundColor: "#E4E4E4",
+    borderRadius: 0,
+    color: "#777777"
   }
 });
 export default Untitled1;
